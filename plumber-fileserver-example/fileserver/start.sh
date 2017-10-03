692afef266c96e68079f941ec2c0357541414976
Hao Hou <ghost89413@gmail.com>
2017-10-03 14:54:28 -0600

    add more  options

--- plumber-fileserver-example/fileserver/start.sh
+++ plumber-fileserver-example/fileserver/start.sh
@@ -5,5 +5,25 @@ then
 else
 	ulimit -n unlimited
 	ulimit -n
-	exec /fileserver/fileserver.pss
+	mkdir -p /var/log/plumber
+	mkdir -p /etc/plumber/log.cfg
+	echo "ERROR /var/log/plumber/error.log ae" >> /etc/log/plumber
+	echo "FATAL /var/log/plumber/error.log ae" >> /etc/log/plumber
+	echo "default /var/log/plumber/info.log a" >> /etc/log/plumber
+
+	port=80
+	root=/fileserver/environment/server_files
+	
+	for arg in $@
+	do
+		case ${arg} in
+			--port=* )
+				port=`echo ${arg} | sed 's/^[^=]*=\(.*\)$/\1/g'`
+			;;
+			--root=* )
+				root=`echo ${arg} | sed 's/^[^=]*=\(.*\)$/\1/g'`
+			;;
+		esac
+	done
+	echo exec /fileserver/fileserver.pss ${port} ${root}
 fi

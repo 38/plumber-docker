7bd851a87f718a82554fef4650bca5347174112c
Hao Hou <ghost89413@gmail.com>
2018-03-08 12:44:14 -0700

    Update the server code

--- plumber-fileserver-example/fileserver/getpath/getpath.c
+++ plumber-fileserver-example/fileserver/getpath/getpath.c
@@ -7,17 +7,28 @@
 #include <string.h>
 #include <module/tls/api.h>
 
-#include <pstd/type.h>
+#include <pstd.h>
 #include <pstd/types/string.h>
+
 typedef struct {
 	pipe_t request;
 	pipe_t path;
 	pipe_t host;
+	pipe_t ac_enc;
 	pipe_t error;
+	pipe_t redir;
 
 	pstd_type_model_t*   type_model;
 	pstd_type_accessor_t path_token;
 	pstd_type_accessor_t host_token;
+	pstd_type_accessor_t ac_enc_token;
+	pstd_type_accessor_t redir_status_acc;
+	pstd_type_accessor_t redir_url_acc;
+
+	uint32_t CONST_MOVED;
+
+	uint32_t upgrade_http:1;
+	uint16_t https_port;
 } servlet_data_t;
 int init(uint32_t argc, char const* const* argv, void* data)
 {
@@ -28,6 +39,8 @@ int init(uint32_t argc, char const* const* argv, void* data)
 	sd->path    = pipe_define("path", PIPE_OUTPUT, "plumber/std/request_local/String");
 	sd->error   = pipe_define("error", PIPE_OUTPUT, NULL);
 	sd->host    = pipe_define("host", PIPE_OUTPUT, "plumber/std/request_local/String");
+	sd->ac_enc  = pipe_define("accept_encoding", PIPE_OUTPUT, "plumber/std/request_local/String");
+	sd->redir   = pipe_define("redir", PIPE_OUTPUT, "plumber/std_servlet/filesystem/readfile/v0/Result");
 
 	if(sd->request == (pipe_t)-1 || sd->path == (pipe_t)-1 || sd->error == (pipe_t)-1)
 	{
@@ -38,6 +51,14 @@ int init(uint32_t argc, char const* const* argv, void* data)
 	sd->type_model = pstd_type_model_new();
 	sd->path_token = pstd_type_model_get_accessor(sd->type_model, sd->path, "token");
 	sd->host_token = pstd_type_model_get_accessor(sd->type_model, sd->host, "token");
+	sd->ac_enc_token = pstd_type_model_get_accessor(sd->type_model, sd->ac_enc, "token");
+	sd->redir_status_acc = pstd_type_model_get_accessor(sd->type_model, sd->redir, "status");
+	sd->redir_url_acc = pstd_type_model_get_accessor(sd->type_model, sd->redir, "redirect.token");
+	
+	PSTD_TYPE_MODEL_ADD_CONST(sd->type_model, sd->redir, "STATUS_MOVED", &sd->CONST_MOVED);
+
+	sd->upgrade_http = 1 & (uint32_t)(pstd_libconf_read_numeric("http.upgrade", 0) != 0);
+	sd->https_port   = (uint16_t)(pstd_libconf_read_numeric("http.upgrade_port", 443));
 
 	return 0;
 }
@@ -73,6 +94,7 @@ typedef struct {
 	int has_data;
 	char path[1024];
 	char host[512];
+	char accept_encoding[512];
 	size_t path_length;
 } _state_t;
 static inline _state_t* _state_new()
@@ -83,6 +105,7 @@ static inline _state_t* _state_new()
 	state->has_data = 0;
 	state->field_name_length = state->field_value_length  = state->path_length = 0;
 	state->host[0] = 0;
+	state->accept_encoding[0] = 0;
 	return state;
 }
 
@@ -95,7 +118,8 @@ static inline int _state_free(_state_t* state)
 int _exec(void* data)
 {
 	servlet_data_t* sd = (servlet_data_t*)data;
-	char buffer[4096];
+	char _buffer[4096];
+	char* buffer = NULL;
 	_state_t* state;
 	size_t rem = 0, sz = 0;
 	
@@ -112,7 +136,21 @@ int _exec(void* data)
 	}
 	for(;state->ps != ERR && state->ps != OK;)
 	{
-		sz = pipe_read(sd->request, buffer, sizeof(buffer));
+		/* Before we actually started, we need to release the previously acquired internal buffer */
+		size_t min_sz;
+		if(buffer != _buffer && buffer != NULL && ERROR_CODE(int) == pipe_data_release_buf(sd->request, buffer, sz))
+			goto READ_ERR;
+
+		sz = sizeof(_buffer);
+		int rc = pipe_data_get_buf(sd->request, sizeof(_buffer), (void const**)&buffer, &min_sz, &sz);
+		if(ERROR_CODE(int) == rc) goto READ_ERR;
+
+		if(rc == 0) 
+		{
+			buffer = _buffer;
+			sz = pipe_read(sd->request, buffer, sizeof(buffer));
+		}
+		
 		if(sz == 0)
 		{
 			if(pipe_eof(sd->request) > 0)
@@ -135,6 +173,7 @@ int _exec(void* data)
 		}
 		else if(sz == ERROR_CODE(size_t))
 		{
+READ_ERR:
 			if(new_state) _state_free(state);
 			return 0;
 		}
@@ -204,13 +243,23 @@ int _exec(void* data)
 				    state->keep_alive = 1;
 				if(strcmp(state->field_name, "host") == 0)
 				    strcpy(state->host, state->field_value);
+
+				if(strcmp(state->field_name, "accept-encoding") == 0)
+					strcpy(state->accept_encoding, state->field_value);
+
 				state->field_name_length = state->field_value_length = 0;
 				state->field_name[0] = state->field_value[0] = 0;
 			}
 
 		}
-		if(state->ps == OK && rem + 1 < sz)
-		    pipe_cntl(sd->request, PIPE_CNTL_EOM, buffer, rem);
+		if(state->ps == OK)
+		{
+		    if(_buffer == buffer && rem + 1 < sz) 
+				pipe_cntl(sd->request, PIPE_CNTL_EOM, buffer, rem);
+			
+			if(_buffer != buffer)
+				pipe_data_release_buf(sd->request, buffer, rem);
+		}
 		if(state->ps == ERR)
 		    state->keep_alive = 0;
 	}
@@ -218,22 +267,43 @@ int _exec(void* data)
 	if(state->keep_alive == 1)
 	    pipe_cntl(sd->request, PIPE_CNTL_SET_FLAG, PIPE_PERSIST);
 
-
 	char itbuf[pstd_type_instance_size(sd->type_model)];
 	pstd_type_instance_t* inst = pstd_type_instance_new(sd->type_model, itbuf);
-
+	
+	const char* modpath = NULL;
+	
 	if(state->ps != OK)
 	{
 		pipe_write(sd->error, "400 Bad Request", 15);
 	}
 	else
 	{
-		LOG_NOTICE("Incoming HTTP Request: %s", state->path);
-		//pipe_write(sd->path, state->path, state->path_length);
-		pstd_string_t* pstr = pstd_string_new(state->path_length + 1);
-		pstd_string_write(pstr, state->path, state->path_length);
-		scope_token_t st = pstd_string_commit(pstr);
-		PSTD_TYPE_INST_WRITE_PRIMITIVE(inst, sd->path_token, st); 
+		static const char tcp_prefix[] = "pipe.tcp";
+		pipe_cntl(sd->request, PIPE_CNTL_MODPATH, &modpath);
+		if(sd->upgrade_http && strncmp(modpath, tcp_prefix, sizeof(tcp_prefix) - 1) == 0)
+		{
+			pstd_string_t* pstr = pstd_string_new(state->path_length + state->path_length + 20);
+			pstd_string_write(pstr, "https://", 8);
+			char* port_begin = strchr(state->host, ':');
+			pstd_string_write(pstr, state->host, port_begin == NULL ? strlen(state->host) : port_begin - state->host);
+			if(sd->https_port != 443)
+				pstd_string_printf(pstr, ":%u", sd->https_port);
+			pstd_string_write(pstr, state->path, state->path_length);
+			scope_token_t st = pstd_string_commit(pstr);
+			PSTD_TYPE_INST_WRITE_PRIMITIVE(inst, sd->redir_url_acc, st);
+			PSTD_TYPE_INST_WRITE_PRIMITIVE(inst, sd->redir_status_acc, sd->CONST_MOVED);
+			goto EXIT;
+		}
+		else
+		{
+			LOG_NOTICE("Incoming HTTP Request: %s", state->path);
+			//pipe_write(sd->path, state->path, state->path_length);
+			pstd_string_t* pstr = pstd_string_new(state->path_length + 1);
+			pstd_string_write(pstr, state->path, state->path_length);
+			scope_token_t st = pstd_string_commit(pstr);
+			PSTD_TYPE_INST_WRITE_PRIMITIVE(inst, sd->path_token, st); 
+		}
+
 	}
 
 	if(state->host[0])
@@ -245,16 +315,27 @@ int _exec(void* data)
 		PSTD_TYPE_INST_WRITE_PRIMITIVE(inst, sd->host_token, st); 
 	}
 
+	if(state->accept_encoding[0])
+	{
+		pstd_string_t* pstr = pstd_string_new(strlen(state->accept_encoding) + 1);
+		pstd_string_write(pstr, state->accept_encoding, strlen(state->accept_encoding));
+		scope_token_t st = pstd_string_commit(pstr);
+		PSTD_TYPE_INST_WRITE_PRIMITIVE(inst, sd->ac_enc_token, st); 
+	}
+
+
 	if(new_state == 1)
 	    _state_free(state);
+EXIT:
 
 	pstd_type_instance_free(inst);
 
 
-#ifdef LOG_DEBUG_ENABLED
-	const char* modpath;
 
-	pipe_cntl(sd->request, PIPE_CNTL_MODPATH, &modpath);
+#ifdef LOG_DEBUG_ENABLED
+	if(NULL == modpath) 
+		pipe_cntl(sd->request, PIPE_CNTL_MODPATH, &modpath);
+	
 	LOG_DEBUG("Modpath = %s", modpath);
 	
 	char protobuf[32] = {};
